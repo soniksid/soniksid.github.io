c5e7b23d418452eeb4d06aa56d7213568d698312
Siddharth Soni <57247354+soniksid@users.noreply.github.com>
2021-01-31 17:34:35 +0530

    Made new changes

--- CS50_C/Basics.c
+++ CS50_C/Basics.c
@@ -8,6 +8,7 @@ int main(void) {
     // sum = x + y;      
     // printf("x = %d + y = %d, sum = %d", x, y, sum);
 
+// ------------------------------------------------------------
 // Dividing two numbers
      // float x, y, result;
      // printf("Type any two numbers:\n");
@@ -15,23 +16,31 @@ int main(void) {
      // result = x/y;
      // printf("your answer is: %f", result);
 
+// ------------------------------------------------------------
 // Conditions to check bigger no.
-int x, y, z;
+// int x, y, z;
 
-	printf("Write any three numbers:\n");
-	scanf("%d %d %d", &x, &y, &z);
-	if (x > y) {
-		else
-			if (x > z)
-		{
-			printf(&x);
-		}
-	if (y > x) {
-			else if (y > z)
-			}
-				printf(&y);
-			}
-	}
-	else
-			printf(&z);
+// 	printf("Write any three numbers:\n");
+// 	scanf("%d %d %d", &x, &y, &z);
+// 	if (x > y && x > z)
+// 		{
+// 			printf("x");
+// 		}
+// 	else if (y > x && y > z)
+// 		{
+// 			printf("y");
+// 		}
+// 	else
+// 			printf("z");
+
+// }
+// -------------------------------------------------------------
+// while loop
+
+int i=0;
+while ( i < 20){
+	printf("Do some work\n");
+	i++;
 }
+}
+
