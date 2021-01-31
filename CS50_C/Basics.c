a70aea86e7e7b29c84478dbebb7a879484825520
Siddharth Soni <57247354+soniksid@users.noreply.github.com>
2021-01-31 16:47:51 +0530

    First chapter

---
+++ CS50_C/Basics.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(void) {    
+
+// // Addig two numbers
+    // int x, y, sum;
+    // printf("Enter two integers: ");
+    // scanf("%d %d", &x, &y);
+    // sum = x + y;      
+    // printf("x = %d + y = %d, sum = %d", x, y, sum);
+
+// Dividing two numbers
+     // float x, y, result;
+     // printf("Type any two numbers:\n");
+     // scanf("%f %f", &x, &y);
+     // result = x/y;
+     // printf("your answer is: %f", result);
+
+// Conditions to check bigger no.
+int x, y, z;
+
+	printf("Write any three numbers:\n");
+	scanf("%d %d %d", &x, &y, &z);
+	if (x > y) {
+		else
+			if (x > z)
+		{
+			printf(&x);
+		}
+	if (y > x) {
+			else if (y > z)
+			}
+				printf(&y);
+			}
+	}
+	else
+			printf(&z);
+}
