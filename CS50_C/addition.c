11a9920c43af94ca2359bdd77b90aecd6d9b5a3e
Siddharth Soni <57247354+soniksid@users.noreply.github.com>
2021-02-03 18:44:58 +0530

    new files add

---
+++ CS50_C/addition.c
@@ -0,0 +1,12 @@
+// Addig two numbers
+
+#include <stdio.h>
+int main(void) {    
+
+    int x, y, sum;
+    printf("Enter two integers:\n ");
+    scanf("%d %d", &x, &y);
+    sum = x + y;      
+    printf("x = %d + y = %d, sum = %d", x, y, sum);
+
+}
